21153ba303dd85b107fcde2dbd5a9af0b88c7f3c
Hamza Malik <hamzamalik94@gmail.com>
2019-07-30 16:43:18 +0500

    Delete travis-decode.sh

--- scripts/travis-decode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-[ ! ${TRAVIS_SECURE_ENV_VARS} ] && exit -1;
-
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out local.secring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out local.pubring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in credentials.enc -out local.credentials.sbt -d
